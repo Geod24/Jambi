bc00f476168e37c52ca40ea8b957210ded4d3c18
Mathias LANG <geod24@gmail.com>
2014-11-15 21:04:46 +0100

    JTE TROLL

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
+#include <string.h>
+
+typedef struct s_String {
+  char* ptr;
+  size_t length;
+} String;
 
 int main(int ac, char** args) {
-  
+  // I miss foreach :(
+  String dir;
+  for (int i = 1; i < ac; ++i) {
+    dir.ptr = args[i];
+    dir.length = strlen(args[i]);
+    
+  }
   return 0;
 }
