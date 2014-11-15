a65ccb3608145d2b84bdb34321bc8ba34c5a0431
Kapp Arnaud <kapp.arno@gmail.com>
2014-11-15 21:04:56 +0100

    display error msg. check magic 1

--- src/test.c
+++ src/test.c
@@ -1,7 +1,15 @@
+#include <assert.h>
 #include "nt-headers.h"
 #include "read_file.h"
+#include "parser.h"
 
-int main()
+int main(int ac, char **av)
 {
-  
+  parser_t *parser;
+
+  assert(ac > 1);
+  parser = parse_from_file(av[1]);
+  assert(parser);
+
+  return 0;
 }
